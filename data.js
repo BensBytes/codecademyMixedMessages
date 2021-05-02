60b97785cfaaece80bff3bf77517113a8a3f0b8f
Benjamin Brandt <babrandt@gmail.com>
2021-05-02 16:04:25 -0400

    Init; data file established

---
+++ data.js
@@ -0,0 +1,162 @@
+const trivia = [
+        {
+            category: 'Animal Trivia',
+            question: 'Native to the coasts of the northern and eastern North Pacific Ocean, which marine mammal is the heaviest member of the weasel family?',
+            answer: 'Sea Otter'
+        },
+        {
+            category: 'Animal Trivia',
+            question: 'Believed to grow as large as 60 feet (18 meters), what is the largest species of shark currently living in the ocean?',
+            answer: 'Whale Shark (Rhincodon typus)'
+        },
+        {
+            category: 'Animal Trivia',
+            question: 'Existing as recently as 100,000 years ago, Gigantopithecus blacki is an extinct species what animal?',
+            answer: 'Ape'
+        },
+        {
+            category: 'Animal Trivia',
+            question: 'Which marine animal is the only known natural predator of the great white shark?',
+            answer: 'Orca (killer whale)'
+        },
+        {
+            category: 'Animal Trivia',
+            question: '“Dazzle” is a term used for a group of what type of animal?',
+            answer: 'Zebra'
+        },
+        {
+            category: 'Chemistry',
+            question: 'On the periodic table, which element has an atomic weight of 1.00794?',
+            answer: 'Hydrogen'
+        },
+        {
+            category: 'Chemistry',
+            question: 'On the periodic table of elements, which element has the shortest name?',
+            answer: 'Tin'
+        },
+        {
+            category: 'Chemistry',
+            question: 'On the Periodic Table of Elements, Hg is the symbol for what element?',
+            answer: 'Mercury'
+        },
+        {
+            category: 'Chemistry',
+            question: 'What is the most abundant chemical element in the Universe?',
+            answer: 'Hydrogen'
+        },
+        {
+            category: 'Chemistry',
+            question: 'Which metallic element has a melting point of approximately −38 °F (−39 °C)?',
+            answer: 'Mercury'
+        },
+        {
+            category: 'Chemistry',
+            question: 'How many electrons does a hydrogen atom have?',
+            answer: 'One'
+        },
+        {
+            category: 'Chemistry',
+            question: 'What chemical element gives the blood of a lobster a bluish tint?',
+            answer: 'Copper'
+        },
+        {
+            category: 'Chemistry',
+            question: 'Bronze is an alloy consisting primarily of what two elements?',
+            answer: 'Copper & Tin'
+        },
+        {
+            category: 'Chemistry',
+            question: 'Which is the most abundant metal in the earth’s crust?',
+            answer: 'Aluminum'
+        },
+        {
+            category: 'Chemistry',
+            question: 'Fe is the chemical symbol for what element?',
+            answer: 'Iron'
+        },
+        {
+            category: 'Computer Trivia',
+            question: 'To celebrate its 30th birthday in 2010, Google placed a playable version of what arcade game on its homepage?',
+            answer: 'Pac-Man'
+        },
+        {
+            category: 'Computer Trivia',
+            question: 'When referring to phone calls made over the internet, what does the acronym VoIP stand for?',
+            answer: 'Voice over Internet Protocol'
+        },
+        {
+            category: 'Computer Trivia',
+            question: 'When referring to a website’s address was does the acronym URL stand for?',
+            answer: 'Uniform Resource Locator'
+        },
+        {
+            category: 'Computer Trivia',
+            question: 'In computer terminology what does the acronym LAN stand for?',
+            answer: 'Local Area Network'
+        },
+        {
+            category: 'Computer Trivia',
+            question: 'What is the only state that can be typed on one row of keys on a QWERTY keyboard?',
+            answer: 'Alaska'
+        },
+        {
+            category: 'Computer Trivia',
+            question: 'What three-digit web error code for censored content is a reference to a Ray Bradbury novel?',
+            answer: '451'
+        },
+        {
+            category: 'Computer Trivia',
+            question: 'What do the letters CPU stand for when referring to the “brains” of a computer?',
+            answer: 'Central Processing Unit'
+        },
+        {
+            category: 'Geography',
+            question: 'Stockholm is the capital and largest city of what country?',
+            answer: 'Sweden'
+        },
+        {
+            category: 'Geography',
+            question: 'Which U.S state located on the eastern seaboard partially falls in the Central Time Zone?',
+            answer: 'Florida'
+        },
+        {
+            category: 'Geography',
+            question: 'In terms of geographical area, what is the largest country in Africa?',
+            answer: 'In terms of geographical area, what is the largest country in Africa?'
+        },
+        {
+            category: 'History',
+            question: 'Born into a family of Dutch Americans, who was the only US President to speak English as a second language?',
+            answer: 'Martin Van Buren'
+        },
+        {
+            category: 'History',
+            question: 'Sappho was an Archaic Greek poet from which Greek island?',
+            answer: 'Lesbos'
+        },
+        {
+            category: 'History',
+            question: 'In October 1582, Pope Gregory XIII introduced what calendar?',
+            answer: 'Gregorian calendar'
+        },
+        {
+            category: 'Military Trivia',
+            question: 'The signing of the Paris Peace Accords officially ended direct U.S. involvement in which War?',
+            answer: 'Vietnam War'
+        },
+        {
+            category: 'Military Trivia',
+            question: 'In 1952 the United States Air Force created Project Blue Book to study what?',
+            answer: 'Unidentified Flying Objects (UFOs)'
+        },
+        {
+            category: 'Military Trivia',
+            question: 'Fort Benning is a United States Army post located in which state?',
+            answer: 'Georgia'
+        },
+    ];
+
+
+const source = 'https://trivia.fyi';
+
+module.exports = {trivia, source};
